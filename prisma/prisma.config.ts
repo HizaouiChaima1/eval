8497353739c6242511a013625e11d6343472ddeb
chaima hizaoui <chaima.hizaoui@isamm.u-manouba.tn>
2026-07-02 16:49:47 +0100

    changement de configuration de prisma

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
-import { resolve } from "path";
+
 import { defineConfig, env } from "prisma/config";
 
-config({ path: resolve(__dirname, "../.env") });
+
 
 export default defineConfig({
     schema: "schema.prisma",
